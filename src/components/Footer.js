e1eb9006aca3f9e3a797913f930c4a682892c6b9
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-13 12:26:13 +0100

    Footer component bare basic component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <span>Logo placeholder Footer</span>
+        <ul>
+          <li>placeholder</li>
+          <li>placeholder</li>
+          <li>placeholder</li>
+        </ul>
+        <p>© 2025</p>
+      </div>
+    </footer>
+  );
+}
