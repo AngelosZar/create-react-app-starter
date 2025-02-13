c031bcbc4deebd4a285f8c758c18f8f128cc5789
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-13 13:03:08 +0100

    intalled tailwind css tested it on header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,17 @@
 export default function Header() {
   return (
-    <header>
-      <div>
+    <header className="flex bg-gray-100">
+      <nav className="flex items-center justify-between w-full max-w-4xl p-4">
         <span>Logo placeholder</span>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Individual product page</li>
-            <li>Cart page</li>
-            <li>Checkout success page</li>
-            <li>Cart page</li>
-            <li>Contact page</li>
-          </ul>
-        </nav>
-      </div>
+        <ul className="flex space-x-4">
+          <li>Home</li>
+          <li>Individual product page</li>
+          <li>Cart page</li>
+          <li>Checkout success page</li>
+          <li>Cart page</li>
+          <li>Contact page</li>
+        </ul>
+      </nav>
     </header>
   );
 }
