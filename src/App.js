7c3a9255532d442decf39462248659f5eae0305a
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-13 17:07:43 +0100

    react router installed and tested on two pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+    // <div className="App">
+    //   <Header />
+    //   <Footer />
+    // </div>
   );
 }
 
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default function Contact() {
+  return (
+    <div>
+      <Header />
+      <h1> I am the contact page look at me </h1>
+      <Footer />
+    </div>
+  );
+}
