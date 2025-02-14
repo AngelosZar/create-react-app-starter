19c2672d1699c2cc6f5074f6da9838af49498e09
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-14 14:43:24 +0100

    working on footer layout

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -13,20 +13,22 @@ import instagramLogo from '../media/Social-media/instagram.png';
 
 export default function Footer() {
   return (
-    <footer className=" bg-gray-100 flex flex-col w-full max-w-1440px mx-auto px-4">
-      <div className="flex items-center justify-between px-4">
-        <div>
+    <footer className=" bg-gray-100 flex flex-row w-full max-w-1440px mx-auto px-4  justify-between">
+      {/* right part of footer */}
+      <div className="flex flex-row justify-between px-4">
+        <div className="w-full">
           <span>
             <Link to="/" className="hover:text-blue-800 transition-colors">
               <img src={RainyDaysLogo} alt="Logo" className="w-28 h-22 py-2" />
             </Link>
           </span>
-          <ul className="flex space-x-4 justify-center items-center">
+          <ul className="flex space-x-4 items-center w-full py-4 justify-center">
             <li>&copy; 2025 ALL RIGHTS RESERVED</li>
             <li>Terms & conditions</li>
             <li>Privacy policy</li>
             <li>Cookie policy</li>
           </ul>
+
           <div className="flex space-x-4 justify-start items-center">
             <img
               src={paypalColorIcon}
@@ -52,8 +54,10 @@ export default function Footer() {
             />
           </div>
         </div>
-        {/* right part of footer */}
-        <div className="flex space-x-4 justify-between items-center">
+      </div>
+      {/* right part of footer */}
+      <div className="flex flex-row px-4 justify-end ">
+        <div className="flex justify-center items-end gap-8 m-6">
           <ul>
             <li>Services</li>
             <li>Shipping</li>
@@ -68,55 +72,55 @@ export default function Footer() {
             <li>Career</li>
           </ul>
         </div>
-      </div>
-      <div className="flex flex-col">
-        <h6>Follow us on</h6>
-        <h6>Social Media</h6>
-        <div>
-          <ul className="flex space-x-4 justify-start items-center">
-            <li>
-              <a
-                href="https://facebook.com/yourpage"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:opacity-80 transition-opacity"
-              >
-                <img
-                  src={facebookIcon}
-                  alt="Facebook Logo"
-                  className="w-12 h-12"
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://pinterest.com/yourpage"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:opacity-80 transition-opacity"
-              >
-                <img
-                  src={pinterestIcon}
-                  alt="Pinterest Logo"
-                  className="w-12 h-12"
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://instagram.com/yourpage"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:opacity-80 transition-opacity"
-              >
-                <img
-                  src={instagramLogo}
-                  alt="Instagram Logo"
-                  className="w-12 h-12"
-                />
-              </a>
-            </li>
-          </ul>
+        <div className="flex flex-row items-end">
+          <div>
+            <h6 className="text-center">Follow us on</h6>
+            <h6 className="text-center">Social Media</h6>
+            <ul className="flex space-x-4 justify-start items-center p-4">
+              <li>
+                <a
+                  href="https://facebook.com/yourpage"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:opacity-80 transition-opacity"
+                >
+                  <img
+                    src={facebookIcon}
+                    alt="Facebook Logo"
+                    className="w-12 h-12"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://pinterest.com/yourpage"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:opacity-80 transition-opacity"
+                >
+                  <img
+                    src={pinterestIcon}
+                    alt="Pinterest Logo"
+                    className="w-12 h-12"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://instagram.com/yourpage"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:opacity-80 transition-opacity"
+                >
+                  <img
+                    src={instagramLogo}
+                    alt="Instagram Logo"
+                    className="w-12 h-12"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
