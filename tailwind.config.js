1a576acc136cf620cd84f25d191639c64f725e7f
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-18 16:02:43 +0100

    added color palete to config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,24 @@ module.exports = {
       maxWidth: {
         1440: '1440px',
       },
+      colors: {
+        body: {
+          dark: '#0B1423',
+          // light: '#F5F5EF',
+          light: '#f1f1f1',
+        },
+        text: {
+          dark: '#000',
+          white: '#fff',
+          grey: '#5e5e5e',
+          blue: '#003049',
+        },
+        blue: {
+          1: '#003049',
+          2: '#014e76',
+          3: '#118ab2',
+        },
+      },
     },
   },
   plugins: [],
