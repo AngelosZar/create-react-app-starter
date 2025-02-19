b0435c0db3cd4f038910cd3202480ff3d442b64f
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-19 11:30:16 +0100

    created functions for the basic content of the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,9 @@ export default function Home() {
   return (
     <Layout>
       <HeroOnHome />
+      <ProductsFeed />
+      <QuoteSection />
+      <TestimonialSection />
     </Layout>
   );
 }
@@ -17,8 +20,66 @@ export default function Home() {
 function HeroOnHome() {
   return (
     <div>
-      <h1>I am the hero section </h1>
-      <p>i am a text how do i look?</p>
+      <h1>hero</h1>
+      <img
+        src="https://images.unsplash.com/photo-1469105692624-86ae1dbf4c23?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="a man on a mountain"
+      />
+      <p>a message to placed fixed on image</p>
+    </div>
+  );
+}
+
+function ProductsFeed() {
+  return (
+    // map the products here // return <ProductCard />
+    <div>
+      <ProductCard />
+    </div>
+  );
+}
+function ProductCard() {
+  return (
+    // grid layout ca 12 cards after use pagination
+    <div>
+      {' '}
+      <div className="max-w-sm flex flex-row">
+        <img
+          src="https://images.unsplash.com/photo-1469105692624-86ae1dbf4c23?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="a man on a mountain"
+        />
+        <div>
+          {' '}
+          <h2>Product title</h2>
+          <p>Product description</p>
+          <p>Price</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function QuoteSection() {
+  return (
+    <div>
+      <h2>Quote</h2>
+      <img
+        src="https://images.unsplash.com/photo-1469105692624-86ae1dbf4c23?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+        srcset=""
+      />
+    </div>
+  );
+}
+
+function TestimonialSection() {
+  return (
+    <div>
+      <h2>Testimonials</h2>
+      <div>
+        <img src="" alt="some person" />
+        <p>Testimonial</p>
+      </div>
     </div>
   );
 }
