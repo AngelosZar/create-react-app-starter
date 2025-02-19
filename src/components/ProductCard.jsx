6717d94ebeaf864c15a399bd93567d955e9d22e6
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-19 14:59:56 +0100

    created ReviewCard component and dynamicaly display it

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -14,13 +14,30 @@ export function ProductCard({ product }) {
           <p>{product.price}</p>
           <p>{product.discountedPrice}</p>
         </div>
-        <div>
-          <p>{product.rating}</p>
-          {/* need to map  */}
-          {/* <p>{product.reviews}</p>
-          <p>{product.category}</p> */}
-        </div>
+        {/* render only if review array has reviews */}
+        {product.reviews.length > 0 && <ReviewCard product={product} />}
+        {/* <ReviewCard product={product} /> */}
       </div>
+    </div>
+  );
+}
+
+function ReviewCard({ product }) {
+  // 0
+  // :
+  // {id: '47af5354-b3be-42aa-8780-503fcabe2f3e', username: 'Emma K.', rating: 5, description: "Love these earrings, they're so elegant and versatile."}
+  return (
+    <div>
+      <h3>Reviews</h3>
+      {product.reviews.map(review => {
+        return (
+          <div key={review.id}>
+            <p>{review.username}</p>
+            <p>{review.rating}</p>
+            <p>{review.description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
