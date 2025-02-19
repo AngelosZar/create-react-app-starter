7c7c3c94d89e77ddcbc2d8a5fdecfe6f341c83ce
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-19 14:47:04 +0100

    displaying basic info on Product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,24 @@
-export function ProductCard() {
+export function ProductCard({ product }) {
   return (
     // grid layout ca 12 cards after use pagination
     <div>
-      {' '}
       <div className="max-w-sm flex flex-row">
         <img
-          src="https://images.unsplash.com/photo-1469105692624-86ae1dbf4c23?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="a man on a mountain"
+          src={product.image.url}
+          alt={product.image?.alt}
+          className="block h-48 w-48"
         />
         <div>
-          {' '}
-          <h2>Product title</h2>
-          <p>Product description</p>
-          <p>Price</p>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>{product.price}</p>
+          <p>{product.discountedPrice}</p>
+        </div>
+        <div>
+          <p>{product.rating}</p>
+          {/* need to map  */}
+          {/* <p>{product.reviews}</p>
+          <p>{product.category}</p> */}
         </div>
       </div>
     </div>
--- src/components/ProductsFeed.jsx
+++ src/components/ProductsFeed.jsx
@@ -1,13 +1,12 @@
 import { ProductCard } from './ProductCard';
 
 export function ProductsFeed({ products }) {
-  // products.map(product => {
-  //   console.log(product);
-  // });
   return (
     // map the products here // return <ProductCard />
-    <div>
-      <ProductCard />
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {products.map(product => {
+        return <ProductCard key={product.id} product={product} />;
+      })}
     </div>
   );
 }
