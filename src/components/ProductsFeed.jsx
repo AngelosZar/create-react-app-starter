5a2db22127d8d80c248bafb8688debf514109359
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-19 14:34:45 +0100

    use effect to fetch data and usestate to save data array.passed it as prompt tp ProductFeed component

--- src/components/ProductsFeed.jsx
+++ src/components/ProductsFeed.jsx
@@ -1,6 +1,9 @@
 import { ProductCard } from './ProductCard';
 
-export function ProductsFeed() {
+export function ProductsFeed({ products }) {
+  // products.map(product => {
+  //   console.log(product);
+  // });
   return (
     // map the products here // return <ProductCard />
     <div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { HeroOnHome } from '../components/HeroOnHome';
 import { QuoteSection } from '../components/QuoteSection';
 import { TestimonialSection } from '../components/TestimonialSection';
 import { SearchBar } from '../components/SearchBar';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 //
 import { ALLPRODUCTSURL } from '../constants/apiEndPoints';
 
@@ -16,15 +16,30 @@ import { ALLPRODUCTSURL } from '../constants/apiEndPoints';
 // The homepage should have a lookahead/auto-complete Search bar component. Typing values in the search bar should display products where the title matches the search input. Clicking on an item should take the user to the ProductPage page. Tip: Filter the user input and then display products that match the input.
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
   useEffect(() => {
-    try {
-    } catch (error) {}
+    async function fetchData() {
+      try {
+        const response = await fetch(ALLPRODUCTSURL);
+        if (!response.ok) {
+          throw new Error(`error ${response.status} `);
+        }
+        console.log(response);
+        const data = await response.json();
+        setProducts(data.data);
+        return data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
   }, []);
+
   return (
     <Layout>
       <HeroOnHome />
       <SearchBar />
-      <ProductsFeed />
+      <ProductsFeed products={products} />
       <QuoteSection />
       <TestimonialSection />
     </Layout>
