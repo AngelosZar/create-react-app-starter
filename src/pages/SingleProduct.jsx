503ded842ed318aea85edfd9c26696a95e055eba
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-19 17:04:18 +0100

    successfully displaying data from the single product

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -2,6 +2,7 @@ import Layout from '../layouts/Layout';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getSingleProductUrl } from '../constants/apiEndPoints';
+//
 export default function SingleProductPage() {
   const [product, setProduct] = useState('');
   const { id } = useParams();
@@ -16,41 +17,37 @@ export default function SingleProductPage() {
           throw new Error(`error ${response.status}`);
         }
         const data = await response.json();
-        setProduct(data);
+        setProduct(data.data);
+        console.log(data);
       } catch (error) {}
     }
     fetchData();
   }, [id]);
   return (
     <Layout>
-      <SingleProduct />
-      <RecommendedProducts />
+      <SingleProduct product={product} />
+      {/* how to add recommended products ? */}
     </Layout>
   );
 }
 
-function SingleProduct() {
+function SingleProduct({ product }) {
+  //  add a loading spinner / or the sceleton thing
+  if (!product) {
+    return <p>Loading...</p>;
+  }
   return (
     <section>
       <div>
-        <img
-          src="https://images.unsplash.com/photo-1541185934-01b600ea069c?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="placeholder"
-          className="w-96 h-96"
-        />
+        <img src={product.image.url} alt="placeholder" className="w-96 h-96" />
         <h1>Single Product Page</h1>
-        <p>image of product </p>
-        <p>Product name</p>
-        <p>Product price</p>
-        <p>Product description</p>
-        <select defaultValue="" name="Size" id="">
-          <option value="" disabled>
-            Select Size
-          </option>
-          <option value="small">Small</option>
-          <option value="md">md</option>
-          <option value="lg">lg</option>
-        </select>
+        <p>{product.title}</p>
+        <p>
+          {product.discountedPrice ? product.discountedPrice : product.price}
+        </p>
+        <p>{product.description}</p>
+        {/*  */}
+        <p>Rating :{product.rating}</p>
         <button>Buy Now</button>
       </div>
     </section>
