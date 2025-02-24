714baedd1c152ac930b7a2c18c3ccf36d96d3fa5
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-24 15:07:19 +0100

    style shanges on cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -16,9 +16,9 @@ export function ProductCard({ product }) {
 
   return (
     // grid layout ca 12 cards after use pagination
-    <div className="bg-white p-4 rounded-lg shadow-md max-w-sm hover:shadow-lg hover:border-blue-3 transition  transform duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:border-y-2 mt-8 flex flex-col mx-4">
+    <div className="bg-white p-4 rounded-lg shadow-md max-w-sm hover:shadow-lg hover:border-blue-3 transition transform duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:border-y-2 mt-8 flex flex-col mx-auto relative items-cente">
       <div
-        className="max-w-sm grid grid-cols-1 md:grid-cols-2 gap-4 cursor-pointer"
+        className="max-w-sm grid grid-cols-1 md:grid-cols-2 gap-4 cursor-pointer "
         onClick={handleClick}
         // key={product.id}
       >
@@ -29,15 +29,13 @@ export function ProductCard({ product }) {
             className="object-cover rounded-lg aspect-square"
           />
         </div>
-        <div className="flex flex-col pl-4 border-l-2 border-blue-3">
+        <div className="flex flex-col pl-4 border-l-2 border-blue-3 relative">
           <h5 className="text-xl pb-4">{product.title}</h5>
-          <p className="">{product.description}</p>
-          {/* <p className="line-clamp-3">{product.description}</p> */}
+          {/* <p className="">{product.description}</p> */}
+          <p className="line-clamp-3 pr-4">{product.description} </p>
+          {/* <span className="absolute bottom-0 right-0">More</span> */}
         </div>
-        {/* render only if review array has reviews */}
-        {/* {product.reviews.length > 0 && <ReviewCard product={product} />} */}
-        {/* <ReviewCard product={product} /> */}
-        {/*  */}
+
         {product.discountedPrice < product.price ? (
           // <div className="flex gap-2 mt-4 flex-col">
           <div className="grid subgrid">
@@ -55,12 +53,7 @@ export function ProductCard({ product }) {
           <p className="mt-4">{product.price} nok</p>
         )}
         {/*test buttons */}
-        <button
-          className="bg-blue-3 text-white rounded-lg p-2 mt-4 self-end justify-self-end"
-          onClick={() => addToCart(product)}
-        >
-          Add to Cart
-        </button>
+
         {/* <span className=" self-end justify-self-end"> </span> */}
       </div>
 
@@ -74,6 +67,12 @@ export function ProductCard({ product }) {
         clear
       </button> */}
       {/*  */}
+      <button
+        className="bg-blue-3 text-white rounded-lg p-2 mt-4 self-end justify-self-end absolute bottom-4 right-4"
+        onClick={() => addToCart(product)}
+      >
+        Add to Cart
+      </button>
     </div>
   );
 }
