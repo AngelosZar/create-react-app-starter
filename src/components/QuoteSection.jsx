8a6e87367ebd329d59b66f1561fc14b2a7c967ff
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-24 15:29:29 +0100

    added image fro blockquote section and a marketing message

--- src/components/QuoteSection.jsx
+++ src/components/QuoteSection.jsx
@@ -1,11 +1,15 @@
+import imgOnQuoteSection from '../media/images/growtika-mlpsHpUUCHY-unsplash.jpg';
 export function QuoteSection() {
   return (
-    <div>
-      <h2>Quote</h2>
-      <img
-        src="https://images.unsplash.com/photo-1469105692624-86ae1dbf4c23?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
-      />
+    <div className="text-center py-20">
+      <blockquote className="italic text-gray-800 border-l-4 border-blue-3 pl-4 my-6 max-w-2xl mx-auto">
+        Transform your lifestyle with our curated collection. From
+        state-of-the-art electronics that power your day to designer shoes that
+        elevate your style, luxurious perfumes that capture unforgettable
+        moments, and advanced skincare that reveals your natural radiance.
+        Everything you desire, all in one destination.
+      </blockquote>
+      <img src={imgOnQuoteSection} alt="" className="max-w-2xl mx-auto" />
     </div>
   );
 }
