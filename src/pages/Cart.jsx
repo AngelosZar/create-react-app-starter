243ae67af55cb1ccdbb2eec70f814dc7dd65307e
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-02-25 19:27:27 +0100

    added cartContext and started working on the CartItem card

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,19 +4,29 @@ import { CartContext } from '../contexts/CartContext';
 //
 export default function Cart() {
   //
-  const { cart, addToCart, removeFromCart, clearCart } =
-    useContext(CartContext);
+  const {
+    cart,
+    addToCart,
+    removeFromCart,
+    clearCart,
+    setCartItems,
+    cartItems,
+    cartTotal,
+  } = useContext(CartContext);
   console.log(cart);
 
   return (
     <Layout>
       <section className="flex flex-col md:flex-row gap-4">
-        <CheckoutForm />
+        {/* <CheckoutForm /> */}
         <OrderSummary
           cart={cart}
           addToCart={addToCart}
           removeFromCart={removeFromCart}
           clearCart={clearCart}
+          setCartItems={setCartItems}
+          cartTotal={cartTotal}
+          cartItems={cartItems}
         />
       </section>
     </Layout>
@@ -37,7 +47,15 @@ function CheckoutForm() {
     </div>
   );
 }
-function OrderSummary({ cart, addToCart, removeFromCart, clearCart }) {
+function OrderSummary({
+  cart,
+  addToCart,
+  removeFromCart,
+  clearCart,
+  cartItems,
+  setCartItems,
+  cartTotal,
+}) {
   // display cart items
   // calculate
   // display total
@@ -47,38 +65,40 @@ function OrderSummary({ cart, addToCart, removeFromCart, clearCart }) {
         cart={cart}
         addToCart={addToCart}
         removeFromCart={removeFromCart}
-      />
-      <CartItem
-        cart={cart}
-        addToCart={addToCart}
-        removeFromCart={removeFromCart}
-      />
-      <CartItem
-        cart={cart}
-        addToCart={addToCart}
-        removeFromCart={removeFromCart}
+        cartTotal={cartTotal}
+        cartItems={cartItems}
       />
       <h3>Order Summary</h3>
-      <p>Subtotal: 20</p>
-      <p>Shipping: 5</p>
-      <p>Total: 25</p>
+      <p>Subtotal: {cartTotal}</p>
+      <p>Shipping: {cartItems} products</p>
       <button onClick={clearCart}>Empty cart</button>
       <br />
       <p>add promo code</p>
     </section>
   );
 }
-function CartItem() {
+function CartItem({ cart, addToCart, removeFromCart, cartTotal, cartItems }) {
   // render product details
   // remove product // add and minus on quantity
-  return (
-    // map cart items
-    <div>
-      <h3>Product Name</h3>
-      <p>Price: 10</p>
-      <p>Quantity: 2</p>
-      <p>Total: 20</p>
-      <button>Remove</button>
+  return cart.map(product => (
+    <div key={product.id}>
+      <h3>{product.title}</h3>
+      <p>Price: {product.price}</p>
+      <p>Quantity: {product.quantity}</p>
+      <p></p>
+      <button onClick={() => addToCart(product)}>+</button>
+      <p>fd</p>
+      <button onClick={() => removeFromCart(product)}>-</button>
     </div>
-  );
+  ));
 }
+
+// </div>)
+// <CartItem />
+// <div>
+//   <h3>Product Name</h3>
+//   <p>Price: 10</p>
+//   <p>Quantity: 2</p>
+//   <p>Total: 20</p>
+//   <button>Remove</button>
+// </div>
