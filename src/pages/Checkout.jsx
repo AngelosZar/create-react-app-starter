2dc64613c1881e6845fcb2dd47e79b700cb4c903
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-03-06 16:27:26 +0100

    breaking down checkout form into smaller components  while styling

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -11,89 +11,137 @@ export default function Checkout() {
   );
 }
 
-// function CheckoutForm() {
-//   return (
-//     // <section className="flex flex-col w-full max-w-lg justify-center items-center mx-auto my-12 p-8 bg-white shadow-md rounded-lg gap-8">
-//     //   <div>{/*  */}</div>
-//     // </section>
-
-//   );
-// }
 function CheckoutForm() {
-  let navigate = useNavigate();
+  const {
+    cart,
+    addToCart,
+    removeFromCart,
+    clearCart,
+    setCartItems,
+    cartItems,
+    cartTotal,
+    deleteFromCart,
+    cartTotalDiscount,
+  } = useContext(CartContext);
+
   // refine form fields
   // Add form validation
   return (
-    <section className="flex flex-col w-full max-w-lg justify-center items-center mx-auto my-12 p-8 bg-white shadow-md rounded-lg gap-8">
-      <div className=" py-1 px-4 text-center">
+    <section className="flex flex-col w-full max-w-2xl justify-center items-center mx-auto my-12 p-8 bg-white shadow-md rounded-lg gap-8">
+      <div className=" py-1 px-4 text-center w-full max-w-xl">
         {/* add summary here */}
-        <h2 className="mb-2 mt-4">Checkout</h2>
-        <p className="mb-6">Fill in your details</p>
-        <form className="flex flex-col gap-4 max-w-xs mx-auto">
-          <div className="flex gap-4">
-            <input
-              type="text"
-              placeholder="First name"
-              className="form-input"
-            />
-            <input type="text" placeholder="Last name" className="form-input" />
-          </div>
-          <input type="text" placeholder="Email" className="form-input " />
-          <input type="text" placeholder="Address" className="form-input " />
-          <div className="flex gap-4">
-            <input type="text" placeholder="City" className="form-input" />
-            <input type="text" placeholder="Zip code" className="form-input " />
-          </div>
-          <div className="flex flex-col gap-4">
-            <h2 className="pb-6">Continue to payment</h2>
-            <fieldset className="flex flex-col gap-4">
-              <div className="flex flex-col border-2 border-blue-3 rounded-lg p-4 gap-4">
-                <span className="flex flex-row gap-2">
-                  <input
-                    type="radio"
-                    id="paypal"
-                    name="payment"
-                    value="paypal"
-                  />
-                  <label htmlFor="paypal">Paypal</label>
-                </span>
-                <h6>Continue with paypal</h6>
-              </div>
-              <div className="flex flex-col border-2 border-blue-3 rounded-lg p-4 gap-4">
-                <span className="flex flex-row gap-2">
-                  <input type="radio" id="vipps" name="payment" value="vipps" />
-                  <label htmlFor="vipps">Vipps</label>
-                </span>
-                <h6>Continue with Vipps</h6>
-              </div>
-              <div className="flex flex-col border-2 border-blue-3 p-4 gap-4 rounded-lg ">
-                <span className="flex flex-row gap-2">
-                  <input type="radio" id="visa" name="payment" value="visa" />
-                  <label htmlFor="visa">Visa</label>
-                </span>
-                <h6>Pay with Visa</h6>
-              </div>
-            </fieldset>
-
-            <div className="flex flex-row gap-4">
-              <button
-                type="submit"
-                className="btn-primary"
-                onClick={() => navigate('/checkout-success')}
-              >
-                Checkout
-              </button>
-              <button
-                type="submit"
-                className="btn-secondary"
-                onClick={() => navigate('/')}
-              >
-                Continue shopping
-              </button>
-            </div>
-          </div>
-        </form>
+        <h1 className=" mt-4 mb-6">Checkout</h1>
+        <CartSummary
+          cartTotal={cartTotal}
+          clearCart={clearCart}
+          cartTotalDiscount={cartTotalDiscount}
+        />
+        <ContinueToPayment />
+        <ShippingDetails />
       </div>
     </section>
+  );
+}
+
+function CartSummary({ cartTotal, clearCart, cartTotalDiscount }) {
+  return (
+    <div className="flex flex-col gap-4 border-2 border-blue-3 rounded-lg p-4 mb-12">
+      <h2 className="self-start">Summary</h2>
+      <hr className="w-[25%] h-1 bg-blue-2 self-start" />
+      <div className="flex flex-row justify-between items-center mt-4">
+        <h3>Sub Total</h3>
+        <h3>{cartTotal.toFixed(2)}</h3>
+      </div>
+      <div className="flex flex-row justify-between items-center">
+        <h3>Discount</h3>
+        <h3>{cartTotalDiscount.toFixed(2)}</h3>
+      </div>
+      <div className="flex flex-row justify-between items-center">
+        <h3>Tax</h3>
+        <h3>0</h3>
+      </div>
+      <div className="flex flex-row justify-between items-center">
+        <h3>Shipping</h3>
+        <h3>0</h3>
+      </div>
+
+      <hr className="w-full h-1 bg-blue-2 self-center" />
+      <div className="flex flex-row justify-between items-center">
+        <h3>Total</h3>
+        <h3>{cartTotal.toFixed(2)}</h3>
+      </div>
+      <button className="btn-primary" onClick={clearCart}>
+        Clear cart
+      </button>
+    </div>
+  );
+}
+
+function ContinueToPayment() {
+  let navigate = useNavigate();
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="pb-6">Continue to payment</h2>
+      <fieldset className="flex flex-row gap-4">
+        <div className="flex flex-col border-2 border-blue-3 rounded-lg p-4 gap-4">
+          <span className="flex flex-row gap-2">
+            <input type="radio" id="paypal" name="payment" value="paypal" />
+            <label htmlFor="paypal">Paypal</label>
+          </span>
+          <h6>Continue with paypal</h6>
+        </div>
+        <div className="flex flex-col border-2 border-blue-3 rounded-lg p-4 gap-4">
+          <span className="flex flex-row gap-2">
+            <input type="radio" id="vipps" name="payment" value="vipps" />
+            <label htmlFor="vipps">Vipps</label>
+          </span>
+          <h6>Continue with Vipps</h6>
+        </div>
+        <div className="flex flex-col border-2 border-blue-3 p-4 gap-4 rounded-lg ">
+          <span className="flex flex-row gap-2">
+            <input type="radio" id="visa" name="payment" value="visa" />
+            <label htmlFor="visa">Visa</label>
+          </span>
+          <h6>Pay with Visa</h6>
+        </div>
+      </fieldset>
+
+      <div className="flex flex-row gap-4">
+        <button
+          type="submit"
+          className="btn-primary"
+          onClick={() => navigate('/checkout-success')}
+        >
+          Checkout
+        </button>
+        <button
+          type="submit"
+          className="btn-secondary"
+          onClick={() => navigate('/')}
+        >
+          Continue shopping
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function ShippingDetails() {
+  return (
+    <>
+      <h2 className="pb-6">Shipping details</h2>
+      <form className="flex flex-col gap-4 max-w-xs mx-auto">
+        <div className="flex gap-4">
+          <input type="text" placeholder="First name" className="form-input" />
+          <input type="text" placeholder="Last name" className="form-input" />
+        </div>
+        <input type="text" placeholder="Email" className="form-input " />
+        <input type="text" placeholder="Address" className="form-input " />
+        <div className="flex gap-4">
+          <input type="text" placeholder="City" className="form-input" />
+          <input type="text" placeholder="Zip code" className="form-input " />
+        </div>
+      </form>
+    </>
   );
 }
