539d7f1c5f3fce87e2501d213b130d3b3cdb8587
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-03-08 18:51:39 +0100

    addedc two functions to confirm delete and clear cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -11,7 +11,7 @@ export default function Cart() {
   return (
     <Layout>
       {cart.length > 0 ? (
-        <section className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-4 w-full">
+        <section className="grid grid-cols-1 md:grid-cols-[2fr_1fr] md:gap-4 w-full">
           <CartItems />
           <CartSummary
             cartTotal={cartTotal}
@@ -64,25 +64,33 @@ function CartItem({
   cartItems,
   deleteFromCart,
 }) {
+  const HandleRemoveItem = product => {
+    // alert('Are you sure you want to delete this item?');
+    if (window.confirm('Are you sure you want to delete this item?')) {
+      deleteFromCart(product);
+    }
+    // deleteFromCart(product);
+  };
   return cart.map(product => (
     <div
       key={product.id}
-      className="flex flex-col gap-4 sm:flex-row justify-between items-center mt-12 border-y-2 border-blue-3 py-1 md:px-4 shadow-lg max-w-lg"
+      // className="flex flex-col gap-4 sm:flex-row justify-between items-center mt-12 border-y-2 border-blue-3 py-1 md:px-4 shadow-lg max-w-lg"
+      className="col-span-full flex justify-center items-center shadow-lg border-y-2 border-blue-3 py-1 md:px-4 gap-4 md:gap-4 md:flex-row md:justify-between md:items-center md:mt-12 md:max-w-lg mx-4"
     >
-      <div className="flex flex-col gap-2 justify-start max-w-44 max-h-auto flex-shrink-1 w-[30%] ">
+      <div className="flex flex-col gap-2 justify-start max-w-44 max-h-auto flex-shrink-1 m-4">
         <img
           src={product.image.url}
           alt={product.title}
           className="object-cover max-w-44 max-h-auto flex-shrink-1 aspect-square "
         />
       </div>
-      <div className="flex flex-col gap-2 max-w-sm justify-start items-center text-center sm:text-start sm:items-start sm:justify-start px-4 pt-8 w-[70%]">
-        <h3>{product.title}</h3>
-        <p className="">{product.description}</p>
+      <div className="flex flex-col gap-2 max-w-sm justify-start items-center text-center sm:text-start sm:items-start sm:justify-start px-4 pt-8 w-[50%]">
+        <h4>{product.title}</h4>
+        {/* <p className="">{product.description}</p> */}
 
         {product.discountedPrice < product.price ? (
           <div className="grid subgrid gap-2 mb-6">
-            <div className="flex gap-2 ">
+            <div className="flex gap-2 flex-wrap">
               <p className="line-through">{product.price} </p>
               <p>{product.discountedPrice} nok</p>
             </div>
@@ -105,9 +113,10 @@ function CartItem({
         <span className="self-end">
           <button
             className="hover:text-red-500"
-            onClick={() => deleteFromCart(product)}
+            onClick={() => {
+              HandleRemoveItem(product);
+            }}
           >
-            {/* display message are you sure you want to delete the item */}
             Remove
           </button>
         </span>
@@ -118,9 +127,15 @@ function CartItem({
 
 function CartSummary({ cartTotal, clearCart, cartTotalDiscount }) {
   const navigate = useNavigate();
+
+  const HandleClearCart = function () {
+    if (window.confirm('Are you sure you want to clear cart?')) {
+      clearCart();
+    }
+  };
   return (
-    <div className="mt-12 border-t-2 border-blue-3 py-1 flex flex-col gap-2 md:px-4 mx-4">
-      <h2 className="text-blue-2">Order Summary</h2>
+    <div className="mt-12 border-t-2 border-blue-3 py-1 flex flex-col gap-2 md:px-4 mx-4 ">
+      <h2 className="text-blue-2 mb-2">Order Summary</h2>
       <p>Subtotal: {Number(cartTotal).toFixed(2)} nok</p>
       <p>Discount: {Math.round(cartTotalDiscount)} nok</p>
       <hr className="w-[30%] h-1 bg-blue-2" />
@@ -129,12 +144,12 @@ function CartSummary({ cartTotal, clearCart, cartTotalDiscount }) {
       <hr className="w-[30%] h-1 bg-blue-2" />
       <h4 className="text-blue-2">Total: {Number(cartTotal).toFixed(2)} nok</h4>
       <button
-        onClick={clearCart}
+        onClick={HandleClearCart}
         className="text-red-500 hover:text-red-700 self-start"
       >
         Empty cart
       </button>
-      <button className="btn-primary" onClick={() => navigate('/Checkout')}>
+      <button className="btn-primary " onClick={() => navigate('/Checkout')}>
         Checkout
       </button>
     </div>
